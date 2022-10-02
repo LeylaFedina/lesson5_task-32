641093633f9d196b6555bf49ee383d96e8f32c8a
Лейла Исмаилова <Trupeshnitzia@gmail.com>
2022-10-02 22:25:35 +0300

    начало решения

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+*/
+
+int [] getRandomArray (int length, int startPoint, int endPoint)
+{
+    int [] resultArray = new int [length];
+    for (int i = 0; i < length; i ++)
+    {
+        resultArray [i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+void printArray (int [] incommingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incommingArray.Length; i++)
+    {
+        Console.Write(incommingArray [i]);
+        if (i < incommingArray.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine ("]");
+
+int [] reverseArray (int [] incommingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incommingArray.Length; i++)
+    {
+         incommingArray [i] = incommingArray [i] * (-1);
+        Console.Write(incommingArray [i]);
+        if (i < incommingArray.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine ("]");
+
+
+}
+
+int [] currentArray = getRandomArray(12,-9,9);
+printArray (currentArray);
+int [] reverseArray = getRandomArray(12,-9,9);
+printArray (reverseArray);
